c74b5ae69ee8025d52d0802349c82ac5c1dd0cda
Pyan <Pyan4444@gmail.com>
2020-08-24 18:21:16 -0400

    Added Console Title

--- ui_lib.py
+++ ui_lib.py
@@ -1,10 +1,14 @@
-import os, time
+import os
+import time
+import ctypes
 
 start_time = time.time()
 Center_Amount = 0
 
+
 def Clear():
     os.system('cls')
+
 
 def Title(value):
     new_title = value.center(Center_Amount)
@@ -12,23 +16,31 @@ def Title(value):
     print(f"|{new_title}|")
     print(f"|{invisible}|")
 
+
 def CreateOption(option):
     new_option = option.center(Center_Amount)
     print(f"|{new_option}|")
 
+
 def Credits(value):
-    value = f"By {value}"    
+    value = f"By {value}"
     new_credits = value.center(Center_Amount)
     invisible = "".center(Center_Amount)
     print(f"|{invisible}|")
     print(f"|{new_credits}|")
 
+
 def TopLines():
-    print("╔" + "-"*Center_Amount + "╗")
+    print("╔" + "-" * Center_Amount + "╗")
+
 
 def BottomLines():
-    print("╚" + "-"*Center_Amount + "╝")
+    print("╚" + "-" * Center_Amount + "╝")
+
+
+def SetTitle(title):
+    ctypes.windll.kernel32.SetConsoleTitleW(title)
+
 
 def Debug():
     print(f"UILib finished in {time.time() - start_time} seconds.")
-        
